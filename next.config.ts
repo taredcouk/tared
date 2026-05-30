8730d6eaa2bd06eb4ec1a67580d7928ea69c42ee
root <admin@tared.co.uk>
2026-05-30 17:34:10 +0000

    Deployment enhancement

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.tared.co.uk",
+          },
+        ],
+        destination: "https://tared.co.uk/:path*",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
